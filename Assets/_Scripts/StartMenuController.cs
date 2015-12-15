e5669776f6ba477435fb1179e17f191b9c567d24
DiabelHanta <faim.khandker@gmail.com>
2015-12-14 22:00:28 -0500

    Commit 14 - Included a menu scene that interacts and allows the game play

---
+++ Assets/_Scripts/StartMenuController.cs
@@ -0,0 +1,52 @@
+﻿/*
+	Script File:		StartMenuController.cs
+	Author:				Khandker Faim Hussain
+	Date Modified:		...
+	Description:		...
+	Revision History:	...
+*/
+using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class StartMenuController : MonoBehaviour 
+{
+	//PUBLIC INSTANCE VAIRABLES
+	public Canvas quitMenu;
+	public Button startText;
+	public Button exitText;
+
+	// Use this for initialization
+	void Start () 
+	{
+		quitMenu = quitMenu.GetComponent<Canvas>();
+		startText = startText.GetComponent<Button>();
+		exitText = exitText.GetComponent<Button> ();
+		quitMenu.enabled = false;
+
+	}
+	
+	public void ExitPressed()
+	{
+		quitMenu.enabled = true;
+		startText.enabled = false;
+		exitText.enabled = false;
+	}
+
+//	public void NoPress()
+//	{
+//		quitMenu.enabled = false;
+//		startText.enabled = false;
+//		exitText.enabled = true;
+//	}
+
+	public void StartLevel()
+	{
+		Application.LoadLevel (1); //Scene name
+	}
+
+	public void ExitGame()
+	{
+		Application.Quit ();
+	}
+}
