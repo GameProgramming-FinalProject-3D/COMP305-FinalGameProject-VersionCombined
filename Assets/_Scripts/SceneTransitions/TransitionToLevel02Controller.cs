6d754115ac01ce7a515256f6e342c5bee096368d
DiabelHanta <faim.khandker@gmail.com>
2015-12-18 19:07:54 -0500

    Commit 17 - Combined Level02 to main project. Level02 contains all assets

---
+++ Assets/_Scripts/SceneTransitions/TransitionToLevel02Controller.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TransitionToLevel02Controller : MonoBehaviour 
+{	
+	//COLLISION METHODS
+	void OnTriggerEnter(Collider other)
+	{
+		if(other.gameObject.CompareTag("Player"))
+		{
+			Application.LoadLevel("GameOver");
+		}
+	}
+}
